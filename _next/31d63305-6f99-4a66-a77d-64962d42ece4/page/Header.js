418d5762fa9e3e91cebf1f9fe4e80f22d6e8155d
nalkanenk <47660980+nalkanenk@users.noreply.github.com>
2019-02-23 10:43:18 +0200

    new build

---
+++ _next/31d63305-6f99-4a66-a77d-64962d42ece4/page/Header.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/Header', function() {
+            var comp = module.exports=webpackJsonp([7],{195:function(e,t,l){e.exports=l(196)},196:function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var o=l(24),u=n(o),a=l(2),c=n(a),r=function(){return c.default.createElement("h1",{className:"jsx-2535817226"},"Nk",c.default.createElement(u.default,{styleId:"2535817226",css:["h1.jsx-2535817226{color:#f2f2f2;background-color:#333;display:inline-block;padding:5px;}"]}))};t.default=r}},[195]);
+            return { page: comp.default }
+          })
+        
